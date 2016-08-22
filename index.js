a6f9fc351e3dd6182b5975f76311d6d50dcda08d
Andres Rios <rianby64@gmail.com>
2016-08-22 09:45:59 +0300

    Add the function rewriteScripts

--- index.js
+++ index.js
@@ -2,33 +2,41 @@
   'use strict';
   var tagContent = 'pseudoimport-html';
 
+  function rewriteScripts(element) {
+
+    var scripts = element.querySelectorAll('script');
+    var i;
+    for (var i = 0; i < scripts.length; i++) {
+      var old_script = scripts[i];
+      var new_script = document.createElement('script');
+
+      // clone text (content)
+      if (old_script.src) {
+        new_script.src = old_script.src;
+      } else if (old_script.text) {
+        new_script.text = old_script.text;
+      }
+
+      // clone all attributes
+      for (var j = 0; j < old_script.attributes.length; j++) {
+        new_script.setAttribute(old_script.attributes[j].name, old_script.attributes[j].value);
+      }
+
+      var parent = old_script.parentNode;
+      parent.replaceChild(new_script, old_script);
+    }
+  }
+
   function pseudoImportHTML(element, url) {
     return fetch(url).then(function(response) {
       return response.text();
     }).then(function(text) {
+//      var temporal = document.createElement(element.tagName); // or whatever!
+//      temporal.innerHTML = text;
+//      rewriteScripts(element);
+
       element.innerHTML = text;
-
-      var scripts = element.querySelectorAll('script');
-      var i;
-      for (var i = 0; i < scripts.length; i++) {
-        var old_script = scripts[i];
-        var new_script = document.createElement('script');
-
-        // clone text (content)
-        if (old_script.src) {
-          new_script.src = old_script.src;
-        } else if (old_script.text) {
-          new_script.text = old_script.text;
-        }
-
-        // clone all attributes
-        for (var j = 0; j < old_script.attributes.length; j++) {
-          new_script.setAttribute(old_script.attributes[j].name, old_script.attributes[j].value);
-        }
-
-        var parent = old_script.parentNode;
-        parent.replaceChild(new_script, old_script);
-      }
+      rewriteScripts(element);
       element.dispatchEvent(new CustomEvent('load'));
 
       return element;
