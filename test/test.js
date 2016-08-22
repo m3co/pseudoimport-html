d04a527d9547fb343750f19a2c530804a25f83d3
Andres Rios <rianby64@gmail.com>
2016-08-22 13:31:17 +0300

    Check if import executes the scripts

--- test/test.js
+++ test/test.js
@@ -4,9 +4,12 @@
   var el = document.querySelector(tagImport);
 
   var async1 = async_test('Check if content was fetched correctly');
+  var async2 = async_test('Check if content executes an script');
 
   el.addEventListener('load', async1.step_func(e => {
     assert_true(el.innerHTML.includes('398v2whwu024jnkjw209j2ijlkwmdlkwm029434j'));
     async1.done();
   }));
+
+  window.async2 = async2;
 })();
