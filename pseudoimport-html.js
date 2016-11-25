82ec3ce65f2b6177dbb7e2a2a808830863f2e143
Andres Rios <rianby64@gmail.com>
2016-11-25 15:41:51 +0300

    Close #14

--- pseudoimport-html.js
+++ pseudoimport-html.js
@@ -1,6 +1,43 @@
 'use strict';
 
-;(function () {
+// Polyfill token from
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/includes#Polyfill
+(function () {
+  if (!Array.prototype.includes) {
+    Array.prototype.includes = function (searchElement /*, fromIndex*/) {
+      if (this == null) {
+        throw new TypeError('Array.prototype.includes called on null or undefined');
+      }
+
+      var O = Object(this);
+      var len = parseInt(O.length, 10) || 0;
+      if (len === 0) {
+        return false;
+      }
+      var n = parseInt(arguments[1], 10) || 0;
+      var k;
+      if (n >= 0) {
+        k = n;
+      } else {
+        k = len + n;
+        if (k < 0) {
+          k = 0;
+        }
+      }
+      var currentElement;
+      while (k < len) {
+        currentElement = O[k];
+        if (searchElement === currentElement || searchElement !== searchElement && currentElement !== currentElement) {
+          // NaN !== NaN
+          return true;
+        }
+        k++;
+      }
+      return false;
+    };
+  }
+})();
+(function () {
   var TAG_CONTENT = 'pseudoimport-html';
   var TAG_CONTENT_SRC = 'pseudoimport-html-src';
   var fetchs = [];
