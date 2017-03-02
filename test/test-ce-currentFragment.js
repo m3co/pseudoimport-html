0a340c0d37750abe57e874de8190d3d4f14e5576
Andres Rios <rianby64@gmail.com>
2017-03-02 23:04:59 +0300

    [TEST] Two fragments have a common url

--- test/test-ce-currentFragment.js
+++ test/test-ce-currentFragment.js
@@ -8,14 +8,11 @@
     // [setup]
     var fragment1 = document.createElement('x-fragment');
     var fragment2 = document.createElement('x-fragment');
+    fragment1.id = 'test-1';
+    fragment2.id = 'test-2';
 
     fragment1.setAttribute('src', 'ce-currentFragment/test-1.html');
     fragment2.setAttribute('src', 'ce-currentFragment/test-2.html');
-
-    window.fixtures = {
-      fragment1: fragment1,
-      fragment2: fragment2
-    };
 
     // [run]
     document.body.appendChild(fragment1);
@@ -28,5 +25,28 @@
       resolve();
     });
 
-  })); }, "Two fragments added via body.appendChild see their document.currentFragment");
+  })); }, 'If two fragments were added via body.appendChild then they see their document.currentFragment');
+
+  promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+    // [setup]
+    var fragment1 = document.createElement('x-fragment');
+    var fragment2 = document.createElement('x-fragment');
+    fragment1.id = 'test-7';
+    fragment2.id = 'test-8';
+
+    fragment1.setAttribute('src', 'ce-currentFragment/test-6.html');
+    fragment2.setAttribute('src', 'ce-currentFragment/test-6.html');
+
+    // [run]
+    document.body.appendChild(fragment1);
+    document.body.appendChild(fragment2);
+
+    Promise.all([
+      fragment1.loaded,
+      fragment2.loaded
+    ]).then(() => {
+      resolve();
+    });
+
+  })); }, 'If two fragments have a common url they see their document.currentFragment');
 })();
