ca19aa50ce1c638ee7b1aaf578731eaba973fc6f
Andres Rios <rianby64@gmail.com>
2017-03-02 19:32:14 +0300

    [TEST] Let's try to append 2 fragments at the same level

--- test/currentFragment/test-1.js
+++ test/currentFragment/test-1.js
@@ -1,1 +1,4 @@
-console.log(1,document.currentFragment);
+'use strict';
+test(() => {
+  assert_equals(document.currentFragment, window.fixtures.fragment1);
+}, 'Fixture 1 see document.currentFragment - fragment1');
---
+++ test/currentFragment/test-2.js
@@ -0,0 +1,4 @@
+'use strict';
+test(() => {
+  assert_equals(document.currentFragment, window.fixtures.fragment2);
+}, 'Fixture 2 see document.currentFragment - fragment2');
