da8d8d0bfc23936e3b68d490f5fe710d9707bda3
Daniyal Gabitov <danuy@list.ru>
2017-03-03 11:33:14 +0300

    [TEST] Add ce-currentFragment-external tests

---
+++ test/ce-currentFragment-external/test-4.js
@@ -0,0 +1,4 @@
+'use strict';
+test(() => {
+  assert_equals(document.currentFragment, document.querySelector('#test-4'));
+}, 'Fixture 4 see document.currentFragment - fragment4');
---
+++ test/test-ce-currentFragment-external.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(() => {
+  /**
+   * Two fragments added via body.appendChild see their document.currentFragment
+   */
+  promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+    // [setup]
+    var fragment1 = document.createElement('x-fragment');
+    var fragment2 = document.createElement('x-fragment');
+    fragment1.id = 'test-1';
+    fragment2.id = 'test-2';
+
+    fragment1.setAttribute('src', 'ce-currentFragment-external/test-1.html');
+    fragment2.setAttribute('src', 'ce-currentFragment-external/test-2.html');
+
+    // [run]
+    document.body.appendChild(fragment1);
+    document.body.appendChild(fragment2);
+
+    Promise.all([
+      fragment1.loaded,
+      fragment2.loaded
+    ]).then(() => {
+      resolve();
+      fragment1.remove();
+      fragment2.remove();
+    });
+
+  })); }, 'If two fragments were added via body.appendChild then they see their document.currentFragment');
+
+  promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+    // [setup]
+    var fragment1 = document.createElement('x-fragment');
+    var fragment2 = document.createElement('x-fragment');
+    fragment1.id = 'test-7';
+    fragment2.id = 'test-8';
+
+    fragment1.setAttribute('src', 'ce-currentFragment-external/test-6.html');
+    fragment2.setAttribute('src', 'ce-currentFragment-external/test-6.html');
+
+    // [run]
+    document.body.appendChild(fragment1);
+    document.body.appendChild(fragment2);
+
+    Promise.all([
+      fragment1.loaded,
+      fragment2.loaded
+    ]).then(() => {
+      resolve();
+      fragment1.remove();
+      fragment2.remove();
+    });
+
+  })); }, 'If two fragments have a common url they see their document.currentFragment');
+})();
