8dcfac6e271e1cc32991d21eb73379d0042046cf
Andres Rios <rianby64@gmail.com>
2017-03-09 20:34:27 +0300

    [TEST] Check delayed insertion at second level

--- test/_layouts/test-ce.js
+++ test/_layouts/test-ce.js
@@ -483,6 +483,44 @@ promise_test(function() { return new Promise(this.step_func((resolve, reject) =>
   document.body.appendChild(fragment);
 })); }, "Check delayed insertion of fragment at first level - repeated");
 
+/**
+ * Check delayed insertion of fragments at second level
+ */
+promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+
+  // [setup]
+  var fragment = document.createElement(nameElement);
+  fragment.setAttribute('src', '/test/fixtures/delay-fixture2.html');
+  window.delayFixture2_resolve = resolve;
+  let handlerError = (e) => {
+    window.removeEventListener('error', handlerError);
+    reject(e.message);
+  };
+  window.addEventListener('error', handlerError);
+
+  // [run]
+  document.body.appendChild(fragment);
+})); }, "Check delayed insertion of fragment at second level");
+
+/**
+ * Check delayed insertion of fragments at second level - repeated
+ */
+promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+
+  // [setup]
+  var fragment = document.createElement(nameElement);
+  fragment.setAttribute('src', '/test/fixtures/delay-fixture2.html');
+  window.delayFixture2_resolve = resolve;
+  let handlerError = (e) => {
+    window.removeEventListener('error', handlerError);
+    reject(e.message);
+  };
+  window.addEventListener('error', handlerError);
+
+  // [run]
+  document.body.appendChild(fragment);
+})); }, "Check delayed insertion of fragment at second level - repeated");
+
 //@@include('../_includes/test-ce-currentFragment.js')
 //@@include('../_includes/test-ce-currentFragment-script-src.js')
 //@@include('../_includes/test-ce-currentFragment-nested.js')
--- test/test-ce.js
+++ test/test-ce.js
@@ -484,6 +484,44 @@ promise_test(function() { return new Promise(this.step_func((resolve, reject) =>
 })); }, "Check delayed insertion of fragment at first level - repeated");
 
 /**
+ * Check delayed insertion of fragments at second level
+ */
+promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+
+  // [setup]
+  var fragment = document.createElement(nameElement);
+  fragment.setAttribute('src', '/test/fixtures/delay-fixture2.html');
+  window.delayFixture2_resolve = resolve;
+  let handlerError = (e) => {
+    window.removeEventListener('error', handlerError);
+    reject(e.message);
+  };
+  window.addEventListener('error', handlerError);
+
+  // [run]
+  document.body.appendChild(fragment);
+})); }, "Check delayed insertion of fragment at second level");
+
+/**
+ * Check delayed insertion of fragments at second level - repeated
+ */
+promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+
+  // [setup]
+  var fragment = document.createElement(nameElement);
+  fragment.setAttribute('src', '/test/fixtures/delay-fixture2.html');
+  window.delayFixture2_resolve = resolve;
+  let handlerError = (e) => {
+    window.removeEventListener('error', handlerError);
+    reject(e.message);
+  };
+  window.addEventListener('error', handlerError);
+
+  // [run]
+  document.body.appendChild(fragment);
+})); }, "Check delayed insertion of fragment at second level - repeated");
+
+/**
  * Two fragments added via body.appendChild see their document.currentFragment
  */
 promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
