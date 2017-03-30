0b77a290695e923693aa06ade165efc8ee65ed87
Andres Rios <rianby64@gmail.com>
2017-03-31 00:47:58 +0300

    [TEST] Close #68

--- test/_includes/test-ce-fetch-relative.js
+++ test/_includes/test-ce-fetch-relative.js
@@ -16,4 +16,20 @@ promise_test(function() { return new Promise(this.step_func((resolve, reject) =>
 
 })); }, 'Test relative path at fetch request');
 
+promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
+  // [setup]
+  var fragment = document.createElement('x-fragment');
+  fragment.id = 'fetch-relative-test-2';
+  fragment.setAttribute('src', 'ce-fetch-relative/test-2.html');
+
+  window.fetchRelativeTest2 = {
+    test: this,
+    resolve: resolve,
+    reject: reject,
+    fragment: fragment
+  };
+  // [run]
+  document.body.appendChild(fragment);
+
+})); }, 'Test relative path at nested fetch request');
 
